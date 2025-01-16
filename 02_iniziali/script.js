c8e09b4500e28fff432d57c6e4453a5cc04a60a5
manuelpezzi <pezzimanuel@gmail.com>
2025-01-16 12:09:18 +0100

    iniziali solved

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -4,10 +4,15 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+function trovaIniziali(nomeArrey) {
+    const iniziali = [];
+    for (let i = 0; i < nomeArrey.length; i++) {
+        iniziali.push(nomeArrey[i].charAt(0).toUpperCase())
+    }
+    return iniziali
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
-
+const iniziali = trovaIniziali(names)
+console.log(iniziali)
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
